ac274a14f0e4f8d70da8261557bea321b12111c1
Nova <heetask@gmail.com>
2025-06-23 17:23:55 +0900

    SSG 3. 동적 경로에 적용하기

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,9 +1,16 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "../../lib/fetch-one-book";
 
-// SSR로 동작
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }, { params: { id: "3" } }],
+    fallback: false,
+  };
+};
+
+// SSG로 동작
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
 
@@ -14,7 +21,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
   };
 };
 
-export default function Page({ book }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Page({ book }: InferGetStaticPropsType<typeof getStaticProps>) {
   if (!book) return "문제가 발생했습니다 다시 시도하세요";
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
