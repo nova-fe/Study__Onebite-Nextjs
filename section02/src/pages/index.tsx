2ce4bc289c25a5d78eb3c0ae1aee8f9a2fa46254
Nova <heetask@gmail.com>
2025-06-23 16:35:38 +0900

    SSR 2. 실습

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,45 +1,44 @@
-// CSS Module
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import style from "./index.module.css";
 import SearchableLayout from "../components/searchable-layout";
-import books from "@/mock/books.json";
 import BookItem from "../components/book-item";
 import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "../lib/fetch-books";
+import fetchRandomBooks from "../lib/fetch-random-books";
 
-// SSR
-export const getServerSideProps = () => {
+// SSR로 동작
+export const getServerSideProps = async () => {
   // 컴포넌트보다 먼저 실행, 컴포넌트에 필요한 데이터를 불러오는 함수
-  console.log("서버사이드 프롭스예요..."); // => 서버 환경에서만 실행됨. 브라우저 콘솔에선 뜨지 않음. 터미널에선 뜸
-  const data = "hello";
+  // Promise.all: 인수로 전달한 배열 안에 들어있는 모든 비동기 함수들을 동시에 실행
+  const [allBooks, recoBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
 
   return {
     // props라는 프로퍼티가 들어있는 객체를 반환
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
 
-export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   // data: getServerSideProps 에서 받아온것
-  console.log(data); // => 총 두번 실행됨.(터미널, 브라우저 콘솔에서 한번씩 뜸.)
-
-  // 브라우저측에서만 실행되는 코드를 작성하고 싶다면 useEffect 사용해서 마운트 시점 이후 되도록...
-  useEffect(() => {
-    console.log(window);
-  }, []);
+  console.log(allBooks); // => 총 두번 실행됨.(터미널, 브라우저 콘솔에서 한번씩 뜸.)
 
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
