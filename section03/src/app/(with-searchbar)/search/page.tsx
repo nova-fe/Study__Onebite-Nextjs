898fd5e07bec7400b0f92568dadb4fc0ab23bc12
Nova <heetask@gmail.com>
2025-06-24 13:54:12 +0900

    네비게이팅

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,15 @@
+import ClientComponent from "../../components/client-component";
+
 export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
   // console.log(await searchParams.q); // http://localhost:3000/search?q=123 로 접속시 /search?q=123
   const { q } = await searchParams;
 
-  return <div>Search 페이지 : {q}</div>;
+  return (
+    <div>
+      Search 페이지 : {q}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
 }
