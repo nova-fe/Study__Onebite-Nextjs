16fb50078c11d53c0f40304ab7e4e5f909fcae47
Nova <heetask@gmail.com>
2025-06-24 16:07:59 +0900

    데이터 캐시

--- section04/next.config.ts
+++ section04/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true, // 넥스트 앱에서 발생하는 모든 데이터 패칭 로그로 출력
+    },
+  },
 };
 
 export default nextConfig;
--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,13 @@ import style from "./page.module.css";
 import { BookData } from "../../types";
 
 async function AllBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  // { cache: "no-store" } : 기본값(캐싱을 하지 않음)
+  // { cache: "force-cache" } : 무조건 캐싱, 한번 호출 이후 다시 호출 X
+  // { next: { revalidate: 3 } } : 3초마다 리빌리데이트
+  // { next: { tags: ['a'] } } : 요청이 들어왔을 때 데이터 최신화
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {
+    cache: "force-cache",
+  });
   if (!response.ok) <div>오류가 발생했습니다.</div>;
   const allBooks: BookData[] = await response.json();
   return (
@@ -16,7 +22,9 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, {
+    next: { revalidate: 3 },
+  });
   if (!response.ok) <div>오류가 발생했습니다.</div>;
   const recoBooks: BookData[] = await response.json();
   return (
