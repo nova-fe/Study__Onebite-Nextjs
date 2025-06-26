100f181e7a9498a33d72c8ccc72d869657e388f3
Nova <heetask@gmail.com>
2025-06-26 13:52:47 +0900

    리뷰 재검증 구현하기

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString(); // 값이 있을 경우 문자열 타입으로 변환(toString())
@@ -12,6 +15,7 @@ export async function createReviewAction(formData: FormData) {
       body: JSON.stringify({ bookId, content, author }),
     });
     console.log(response.status);
+    revalidatePath(`/book/${bookId}`); // 재검증 요청
   } catch (err) {
     console.error(err);
     return;
