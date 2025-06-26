f680d150d472b810b23f7f9cef630c18f4325f25
Nova <heetask@gmail.com>
2025-06-26 14:27:21 +0900

    클라이언트 컴포넌트에서의 서버액션

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,38 +1,50 @@
 "use server";
 
 import { revalidatePath, revalidateTag } from "next/cache";
+import { delay } from "../util/delay";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString(); // 값이 있을 경우 문자열 타입으로 변환(toString())
   const author = formData.get("author")?.toString();
 
-  if (!bookId || !content || !author) return;
+  if (!bookId || !content || !author) {
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
+  }
 
   try {
+    await delay(2000);
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
       method: "POST",
       body: JSON.stringify({ bookId, content, author }),
     });
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
     // revalidatePath() : 재검증 요청
     // 1. 특정 주소의 해당하는 페이지만 재검증
     // revalidatePath(`/book/${bookId}`);
-
     // 2. 특정 경로의 모든 동적 페이지를 재검증
     // revalidatePath(`/book/[id]`, "page");
-
     // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
     // revalidatePath("/(with-searchbar)", "layout");
-
     // 4. 모든 데이터 재검증
     // revalidatePath("/", "layout");
-
     // 5. 태그 기준, 데이터 캐시 재검증(가장 효율적)
     revalidateTag(`review-${bookId}`);
+
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
