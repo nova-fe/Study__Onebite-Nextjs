7aa564cf31a401126630c41ecededfa3be3a63f9
Nova <heetask@gmail.com>
2025-06-30 11:01:43 +0900

    패럴랠 & 인터셉팅 라우트

--- section08/eslint.config.mjs
+++ section08/eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off", // 사용하지 않는 변수 경고 표시
-      "@typescript-eslint/no-explicit-any": "warn", // any 타입을 명시적으로 정의할 수 있도록 허용
+      "@typescript-eslint/no-explicit-any": "off", // any 타입을 명시적으로 정의할 수 있도록 허용
     },
   },
 ];
--- section08/src/app/@modal/(.)book/[id]/page.tsx
+++ section08/src/app/@modal/(.)book/[id]/page.tsx
@@ -1,10 +1,9 @@
 import BookPage from "@/app/book/[id]/page";
-import Modal from "../../../components/modal";
+import Modal from "../../../../components/modal";
 
 export default function Page(props: any) {
   return (
     <div>
-      클라이언트 사이드 렌더링 가로채기 성공!
       <Modal>
         <BookPage {...props} />
       </Modal>
